cc871a5c08bb00b4c9ffbe8eeacbb3adb9a2f005
Som Dengre <somdengre852979@gmail.com>
2022-09-25 17:12:59 +0530

    Create database models

---
+++ server/models/chef.js
@@ -0,0 +1,12 @@
+class Chef {
+  constructor(uid, name, profilePicture, pricing, foodTypes) {
+    this.uid = uid;
+    this.name = name;
+    this.isChef = true;
+    this.profilePicture = profilePicture;
+    this.pricing = pricing;
+    this.foodTypes = foodTypes;
+  }
+}
+
+export default Chef;
---
+++ server/models/user.js
@@ -0,0 +1,11 @@
+class User {
+  constructor(uid, name, phone, address, email) {
+    this.uid = uid;
+    this.name = name;
+    this.phone = phone;
+    this.address = address;
+    this.email = email;
+  }
+}
+
+export default User;
