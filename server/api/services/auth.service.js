85158330855383a6b29eac5133230e8f9fb49e3c
Som Dengre <somdengre852979@gmail.com>
2022-09-27 01:47:10 +0530

    Integrated hellossign on chef signup

--- server/api/services/auth.service.js
+++ server/api/services/auth.service.js
@@ -1,6 +1,26 @@
 import { database } from '../../common/firebase';
 import l from '../../common/logger';
 
+// import * as HelloSignSDK from 'hellosign-sdk';
+const HELLOSIGN_CLIENT_ID = '82f4b30c336f28a6a32849b208d4c2ff';
+const HELLOSIGN_API_KEY =
+  '723d0a84cc0aaf41468ce9725cccbae9aa719828569364e398700f3b9b4ef312';
+const HELLOSIGN_TEMPLATE_ID = '25bd3cdbe21ce74730f57ca089a23087dec2dc42';
+
+var hellosign = require('hellosign-sdk')({
+  key: HELLOSIGN_API_KEY,
+  client_id: HELLOSIGN_CLIENT_ID,
+});
+
+const CHEF_ROLE = 'CHEF';
+// const api = new HelloSignSDK.SignatureRequestApi();
+
+// Configure HTTP basic authorization: api_key
+// api.username = HELLOSIGN_API_KEY;
+
+// or, configure Bearer (JWT) authorization: oauth2
+// $config->setAccessToken("YOUR_ACCESS_TOKEN");
+
 class AuthService {
   userCollectionRef = database.collection('users');
   chefCollectionRef = database.collection('chefs');
@@ -25,6 +45,72 @@ class AuthService {
     try {
       const user = await this.userCollectionRef.doc(uid).get();
       if (user.exists) throw new Error('Already signed up as a foodie');
+
+      //
+
+      // const signer1 = {
+      //   role: CHEF_ROLE,
+      //   emailAddress: email,
+      //   name: name,
+      // };
+
+      // const signingOptions = {
+      //   draw: true,
+      //   type: true,
+      //   upload: true,
+      //   phone: false,
+      //   defaultType: 'draw',
+      // };
+
+      // const data = {
+      //   clientId: HELLOSIGN_CLIENT_ID,
+      //   templateIds: [HELLOSIGN_TEMPLATE_ID],
+      //   subject: 'Food safety and standards agreement',
+      //   message: 'Glad we could come to an agreement.',
+      //   signers: [signer1],
+      //   signingOptions,
+      //   testMode: true,
+      // };
+      // const result = await api.signatureRequestCreateEmbeddedWithTemplate(data);
+
+      var signers = [
+        {
+          email_address: email,
+          name: name,
+          role: CHEF_ROLE,
+        },
+      ];
+
+      var options = {
+        test_mode: 1,
+        template_id: [HELLOSIGN_TEMPLATE_ID],
+        subject: 'Food safety and standards agreement',
+        message: 'Glad we could come to an agreement.',
+
+        signers: signers,
+        // custom_fields: [
+        //   {
+        //     name: 'start_date',
+        //     value: '01/10/2016',
+        //     editor: 'Signer',
+        //     required: true,
+        //   },
+        // ],
+      };
+
+      const result =
+        await hellosign.signatureRequest.createEmbeddedWithTemplate(options);
+      const signature_id = result.signature_request.signatures[0].signature_id;
+      const embedded_signature_url_res = await hellosign.embedded.getSignUrl(
+        signature_id
+      );
+
+      const embedded_signature_url =
+        embedded_signature_url_res.embedded.sign_url;
+
+      console.log('embedded_signature_url', embedded_signature_url);
+      console.log(result);
+
       await this.chefCollectionRef.doc(uid).set({
         name,
         phone,
@@ -42,8 +128,15 @@ class AuthService {
         email,
         isChef: true,
         createdAt: new Date(),
+        signature_request: result.signature_request,
+        signature_url: embedded_signature_url,
+        hasSigned: false,
       });
-      return { message: 'Chef registered successfully' };
+      return {
+        message: 'Chef registered successfully',
+        signature_request: result.signature_request,
+        signature_url: embedded_signature_url,
+      };
     } catch (error) {
       l.error('[SIGNUP SERVICE CHEF]', error);
       throw error;
