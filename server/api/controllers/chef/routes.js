12ef06178adde364b30678189263d155f3bf9bd9
Som Dengre <somdengre852979@gmail.com>
2022-09-27 01:47:52 +0530

    Added route for setSignedContractSuccess

--- server/api/controllers/chef/routes.js
+++ server/api/controllers/chef/routes.js
@@ -6,6 +6,7 @@ export default express
   .Router()
   .get('/', controller.getAllChefDetails)
   .put('/updateProfilePicture', authHandler, controller.updateProfilePicture)
+  .put('/setSignedContractSuccess', authHandler, controller.setSignedContractSuccess)
   .put('/updateProfileData', authHandler, controller.updateProfileData)
   .post('/menuItems', authHandler, controller.createMenuItem)
   .put('/menuItems', authHandler, controller.updateMenuItem);
